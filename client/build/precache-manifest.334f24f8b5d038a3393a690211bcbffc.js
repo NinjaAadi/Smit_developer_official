b236ffdea3e1fad63c663cfe78800e1c8772826f
Aaditya <aaditya7739008423@gmail.com>
2021-10-20 09:57:35 +0530

    Build

---
+++ client/build/precache-manifest.334f24f8b5d038a3393a690211bcbffc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f79b072b4d7cb3d1486066435fe3370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75ca99225204c61f55a6",
+    "url": "/static/css/main.b070f17d.chunk.css"
+  },
+  {
+    "revision": "07fb0348e2e0eae61a2f",
+    "url": "/static/js/2.b45c75e7.chunk.js"
+  },
+  {
+    "revision": "98a3c2222f8cb59385bf407285ce8ab8",
+    "url": "/static/js/2.b45c75e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ca99225204c61f55a6",
+    "url": "/static/js/main.62988f55.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "6475b0fd38ca816739fa5beb5e54a518",
+    "url": "/static/media/SMITDevBlock_logo.6475b0fd.svg"
+  },
+  {
+    "revision": "8a734a33bf6f9917f6f76373b77cab25",
+    "url": "/static/media/background.8a734a33.jpg"
+  },
+  {
+    "revision": "b9f74080afe187bab335e07087c7fd96",
+    "url": "/static/media/blocks_animations.b9f74080.svg"
+  },
+  {
+    "revision": "722816b081cedffbbfa555dc7d81055b",
+    "url": "/static/media/comm.722816b0.svg"
+  },
+  {
+    "revision": "5f52d60844bd4b610e6701991dc26dc4",
+    "url": "/static/media/mid.5f52d608.svg"
+  },
+  {
+    "revision": "f4976c0c8a304ca4abe8e1181cba95e6",
+    "url": "/static/media/profile.f4976c0c.svg"
+  },
+  {
+    "revision": "d9c3589bd3d7d40e33170259254aa79f",
+    "url": "/static/media/profilephoto.d9c3589b.svg"
+  },
+  {
+    "revision": "b856488e163a8b994fb7fbc688f1ff33",
+    "url": "/static/media/side1.b856488e.svg"
+  },
+  {
+    "revision": "e08187a9f20d9bfe41dc7ef20a4a46fe",
+    "url": "/static/media/side2.e08187a9.svg"
+  },
+  {
+    "revision": "e43fea524ddbc2aee30b2df661da2e98",
+    "url": "/static/media/spinner.e43fea52.gif"
+  }
+]);
